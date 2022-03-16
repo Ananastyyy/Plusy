2da20e5701e48829577c85c3d412c13860a6b525
Anastasia <91197697+NovikovaAnastasia@users.noreply.github.com>
2022-03-16 18:13:20 +0500

    Update z2.cpp

--- Deadline_19.03.22/z2.cpp
+++ Deadline_19.03.22/z2.cpp
@@ -10,16 +10,12 @@ int main()
 	list<int>ran;
 	srand(time(NULL));
 	double sum = 0;
-	for (int i = 0; i < 10; ++i) {
+	for (int i = 0; i < 10000; ++i) {
 		int a = -100 + rand() % 201;
 		ran.push_back(a);
 		sum += a;
-		cout << a << " ";
 	}
-	double res = sum/10;
-	cout <<"-------- "<< endl;
-	cout << sum << endl;
-	cout << ran.size() << endl;
+	double res = sum/10000;
 	cout << res << endl;	
 }
 
